34e310c42a162c5101ed1cef64c3ca7c037e8faa
Stefan Pajka <stefpaj@o2.pl>
2018-09-19 02:48:16 +0200

    Zmiana koncepcji, pierwsze prace

---
+++ BotFactory/Assets/Scripts/MainBaseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBaseController : MonoBehaviour {
+
+    public Transform SpawnPoint;
+    public GameObject Unit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnUnit()
+    {
+        Instantiate(Unit, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
---
+++ BotFactory/Assets/Scripts/UnitCreatorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCreatorController : MonoBehaviour {
+
+    public GameObject Top;
+    public GameObject Bot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeBot(GameObject Prefab)
+    {
+        GameObject BotPart = Instantiate(Prefab);
+        BotPart.transform.position = Bot.transform.position;
+        BotPart.transform.rotation = Bot.transform.rotation;
+        BotPart.transform.parent = Bot.transform.parent;
+        Destroy(Bot);
+        Bot = BotPart;
+    }
+
+    public void ChangeTop(GameObject Prefab)
+    {
+        GameObject TopPart = Instantiate(Prefab);
+        TopPart.transform.position = Top.transform.position;
+        TopPart.transform.rotation = Top.transform.rotation;
+        TopPart.transform.parent = Top.transform.parent;
+        Destroy(Top);
+        Top = TopPart;
+    }
+}
